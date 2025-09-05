175715eeff858c117fc95b7a40654afc4c3935d3
rahulnanda15 <141077358+rahulnanda15@users.noreply.github.com>
2025-09-05 01:11:25 -0400

    Fix security issue

--- src/components/ApplicantSearchInput.tsx
+++ src/components/ApplicantSearchInput.tsx
@@ -8,49 +8,13 @@ interface ApplicantSearchInputProps {
 }
 
 export function ApplicantSearchInput({ rusheeName, setRusheeName, applicants }: ApplicantSearchInputProps) {
-    // pat5MZBuJCCU105MQ.8bcbff2a02b3a16fd465d2f1a152f87d96164ebaf598fbe689eac8bb746a4d2f
-    /*
-    useEffect(() => {
-        var Airtable = require('airtable');
-        Airtable.configure({
-            endpointUrl: 'https://api.airtable.com',
-            apiKey: 'pat5MZBuJCCU105MQ.8bcbff2a02b3a16fd465d2f1a152f87d96164ebaf598fbe689eac8bb746a4d2f'
-        });
-        const base = Airtable.base('appEmD27JyhYr4osO');
-    }, []);
-    */
-    //const [applicants, setApplicants] = useState<string[]>([]);
+    
     const [value, setValue] = useState("");
     
     // filter applicants based on `value`:
     const filtered = applicants.filter(
       app => app.toLowerCase().includes(value.toLowerCase())
     );
-    /*
-    useEffect(() => {
-        
-        async function fetchData() {
-            try {
-                const res = await fetch('https://api.airtable.com/v0/appEmD27JyhYr4osO/Applicants', {
-                    headers: { Authorization: `Bearer pat5MZBuJCCU105MQ.8bcbff2a02b3a16fd465d2f1a152f87d96164ebaf598fbe689eac8bb746a4d2f` }     
-                })
-                const data = await res.json();
-                console.log(data.records);
-
-                const applications = data.records;
-
-                const names = applications
-                    .map((app: any) => app.fields.applicant_name)
-                    .filter((name: any) => typeof name === "string");
-
-                setApplicants(names);
-                } catch (err) {
-                console.error("Error fetching data", err);
-                }
-            }
-
-            fetchData();
-            }, []);*/
     
     return (
         <div>
